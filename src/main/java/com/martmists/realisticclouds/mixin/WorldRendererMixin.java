520e88cc8655070ddc3f175de47a85d91b94cf26
Martmists <martmists@gmail.com>
2019-11-30 01:12:10 +0100

    More fix attempts

--- src/main/java/com/martmists/realisticclouds/mixin/WorldRendererMixin.java
+++ src/main/java/com/martmists/realisticclouds/mixin/WorldRendererMixin.java
@@ -3,8 +3,11 @@ package com.martmists.realisticclouds.mixin;
 import com.martmists.realisticclouds.WorldRendererMixinKt;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.util.GlAllocationUtils;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,15 +19,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 class WorldRendererMixin {
     @Shadow
     private ClientWorld world;
+    @Shadow
+    private int cloudsDisplayList;
+    @Shadow
+    private TextureManager textureManager;
 
     @Inject(method = "renderClouds(FDDD)V",
             at = @At("HEAD"), cancellable = true)
     private void renderClouds(float tickDelta, double x, double y, double z, CallbackInfo ci) {
         if (world.dimension.getType() == DimensionType.OVERWORLD) {
+            if (this.cloudsDisplayList >= 0) {
+                GlAllocationUtils.deleteSingletonList(this.cloudsDisplayList);
+                this.cloudsDisplayList = -1;
+            }
+
             WorldRendererMixinKt.tessellator = Tessellator.getInstance();
+            this.cloudsDisplayList = GlAllocationUtils.genLists(1);
+            GlStateManager.newList(this.cloudsDisplayList, 4864);
             WorldRendererMixinKt.renderClouds(tickDelta, x, y - 1, z, world);
-            ci.cancel();
             WorldRendererMixinKt.tessellator.draw();
+            GlStateManager.endList();
+            GlStateManager.disableCull();
+            this.textureManager.bindTexture(new Identifier("textures/environment/clouds.png"));
+            GlStateManager.enableBlend();
+            GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            GlStateManager.pushMatrix();
+            GlStateManager.colorMask(true, true, true, true);
+            GlStateManager.callList(this.cloudsDisplayList);
+            GlStateManager.popMatrix();
+            GlStateManager.clearCurrentColor();
+            GlStateManager.disableBlend();
+            GlStateManager.enableCull();
+            ci.cancel();
         }
     }
 }
--- src/main/kotlin/com/martmists/realisticclouds/WorldRendererMixinKt.kt
+++ src/main/kotlin/com/martmists/realisticclouds/WorldRendererMixinKt.kt
@@ -19,10 +19,10 @@ object WorldRendererMixinKt {
     fun renderClouds(tickDelta: Float, x: Double, y: Double, z: Double, world: ClientWorld) {
         playerPosition = Vec3d(x, y, z)
         builder = tessellator.bufferBuilder
-        builder.begin(GL_QUADS, VertexFormats.POSITION_UV_COLOR)
+        // builder.begin(GL_QUADS, VertexFormats.POSITION_UV_COLOR)
 
         // xyz is player position
-        val r = 20  // radius around player
+        val r = 2  // radius around player
         for (dx in -r..r) {
             val xPos = x + dx
             for (dz in -r..r) {
@@ -44,41 +44,42 @@ object WorldRendererMixinKt {
         val maxDistance = y - world.seaLevel
         val currentDistance = top - world.seaLevel
         val transparency = min(currentDistance / maxDistance, 1.0f)
-        renderCloud(x.toDouble()- playerPosition.x, y.toDouble()-playerPosition.y, z.toDouble()- playerPosition.z, transparency)
+        renderCloud(x.toDouble()- playerPosition.x, y.toDouble()- playerPosition.y, z.toDouble()- playerPosition.z, transparency)
     }
 
     private fun renderCloud(x: Double, y: Double, z: Double, transparency: Float) {
         // TODO: Offset by tickDelta
         // Face 1 (x, y)
         val transparency = (transparency*255).roundToInt()
+        val d = 1
         builder.vertex(x,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y+1,z).color(255, 255, 255, transparency).next()
-        // Face 2 (x, y, +1)
-        builder.vertex(x,y,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y+1,z+1).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y+d,z).color(255, 255, 255, transparency).next()
+        // Face 2 (x, y, +d)
+        builder.vertex(x,y,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y+d,z+d).color(255, 255, 255, transparency).next()
         // Face 3 (x, z)
         builder.vertex(x,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y,z+1).color(255, 255, 255, transparency).next()
-        // Face 4 (x, z, +1)
-        builder.vertex(x,y+1,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y+1,z+1).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y,z+d).color(255, 255, 255, transparency).next()
+        // Face 4 (x, z, +d)
+        builder.vertex(x,y+d,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y+d,z+d).color(255, 255, 255, transparency).next()
         // Face 5 (y, z)
         builder.vertex(x,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y+1,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y+1,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x,y,z+1).color(255, 255, 255, transparency).next()
-        // Face 6 (y, z, +1)
-        builder.vertex(x+1,y,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y+1,z+1).color(255, 255, 255, transparency).next()
-        builder.vertex(x+1,y,z+1).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y+d,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y+d,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x,y,z+d).color(255, 255, 255, transparency).next()
+        // Face 6 (y, z, +d)
+        builder.vertex(x+d,y,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y+d,z+d).color(255, 255, 255, transparency).next()
+        builder.vertex(x+d,y,z+d).color(255, 255, 255, transparency).next()
     }
 }
