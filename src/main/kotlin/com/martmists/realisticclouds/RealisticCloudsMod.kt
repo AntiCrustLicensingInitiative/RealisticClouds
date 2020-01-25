30efb33bb6927f4b346b8e4180318bf623167a00
coderbot <coderbot16@gmail.com>
2020-01-25 10:42:18 -0800

    Update to 1.15.2, remove logspam, fix versions and mod json

--- src/main/kotlin/com/martmists/realisticclouds/RealisticCloudsMod.kt
+++
@@ -1,9 +0,0 @@
-package com.martmists.realisticclouds
-
-import net.fabricmc.api.ClientModInitializer
-
-class RealisticCloudsMod : ClientModInitializer {
-    override fun onInitializeClient() {
-        println("RealisticClouds loaded!")
-    }
-}
